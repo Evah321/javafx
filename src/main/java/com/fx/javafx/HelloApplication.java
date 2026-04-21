dd47b1a799d7ca153b82e3405086b836e4f40555
lumbasi-wabomba <lumbasiwk@gmail.com>
2026-04-21 09:06:17 +0300

    update on config file --changed from using resource file to build everything on one file

--- src/main/java/com/fx/javafx/HelloApplication.java
+++ src/main/java/com/fx/javafx/HelloApplication.java
@@ -45,4 +45,7 @@ public class HelloApplication extends Application {
         stage.setScene(scene);
         stage.show();
     }
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
